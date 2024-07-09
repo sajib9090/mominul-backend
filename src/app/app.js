7f00227006ce2c4f1d56a1630ac31a111a6bbbef
sajib9090 <nispap.devil.sajib@gmail.com>
2024-07-09 17:05:07 +0600

    update cors

--- src/app/app.js
+++ src/app/app.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 //middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://thoughts-app-v1.web.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
